7277826e6300bdbb5ef6b37e70c27300fa80b0a8
muhannad_alshareef <78313215+Muhannad-Alshareef@users.noreply.github.com>
2021-07-03 12:12:49 +0300

    Update wheels driver arduino code.ino

--- wheels driver arduino code.ino
+++ wheels driver arduino code.ino
@@ -1,19 +1,22 @@
-int b1 = 0;
-int b2 = 0;
+int b1 = 0;   //first button input
+int b2 = 0;   //second button input
 void setup() {
-  
-  pinMode(9,OUTPUT);
+  //output pins to connect to the driver
+  pinMode(9,OUTPUT);  
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
   pinMode(12,OUTPUT);
+  //input pins for forward and reverse drive
   pinMode(3,INPUT);
   pinMode(4,INPUT);
   
 }
 
 void loop() {
+ //read the drive command 
   b1=digitalRead(3);
   b2=digitalRead(4);
+  //forward or reverse if statement 
   if(b1==HIGH){
     digitalWrite(9,LOW);
     digitalWrite(11,LOW); 
